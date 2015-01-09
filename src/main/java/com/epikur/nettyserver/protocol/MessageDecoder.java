37bd38a508f2851af7ad0fa7c0e3172ee4e5b21e
shmutalov <shmutalov@gmail.com>
2015-01-09 13:26:44 +0500

    Added MessageDecoder

---
+++ src/main/java/com/epikur/nettyserver/protocol/MessageDecoder.java
@@ -0,0 +1,49 @@
+package com.epikur.nettyserver.protocol;
+
+import java.util.List;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.ReplayingDecoder;
+
+public class MessageDecoder extends ReplayingDecoder<MessageDecoder.DecodingState> {
+
+	public enum DecodingState {
+		VERSION
+		, TYPE
+		, PAYLOAD_LENGTH
+		, PAYLOAD
+	}
+
+	private byte version;
+	private byte type;
+	private int payload_length;
+	
+	public MessageDecoder () {
+		super(MessageDecoder.DecodingState.VERSION);
+	}
+	
+	@Override
+	protected void decode(ChannelHandlerContext ctx, ByteBuf buf,
+			List<Object> out) throws Exception {
+		switch (this.state()) {
+		case VERSION:
+			version = buf.readByte();
+			checkpoint(DecodingState.TYPE);
+		case TYPE:
+			type = buf.readByte();
+			checkpoint(DecodingState.PAYLOAD_LENGTH);
+		case PAYLOAD_LENGTH:
+			payload_length = buf.readInt();
+			checkpoint(DecodingState.PAYLOAD);
+		case PAYLOAD:
+			ByteBuf payload_buf = buf.readBytes(payload_length);
+			checkpoint(DecodingState.VERSION);
+			out.add(new Message(new ProtocolVersion(version), MessageType.fromByte(type), payload_buf.array()));
+			break;
+		default:
+			throw new Error("Shouldn't reach here.");
+		}
+	}
+
+}
