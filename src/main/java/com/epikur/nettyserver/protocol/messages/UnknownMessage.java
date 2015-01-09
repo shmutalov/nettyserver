68aacac2f5d06a666e70dd31a4437fe704cae545
shmutalov <shmutalov@gmail.com>
2015-01-09 14:09:16 +0500

    Implemented <UnknownMessage>

--- src/main/java/com/epikur/nettyserver/protocol/messages/UnknownMessage.java
+++ src/main/java/com/epikur/nettyserver/protocol/messages/UnknownMessage.java
@@ -1,17 +1,22 @@
 package com.epikur.nettyserver.protocol.messages;
 
 import com.epikur.nettyserver.protocol.Packet;
+import com.epikur.nettyserver.protocol.PacketType;
+import com.epikur.nettyserver.protocol.ProtocolVersion;
 
 public class UnknownMessage implements Message {
-
+	private byte [] payload;
+	
 	public void decodePacket(Packet p) {
-		// TODO Auto-generated method stub
-
+		payload = p.getPayload();
 	}
 
 	public Packet encode() {
-		// TODO Auto-generated method stub
-		return null;
+		Packet p = new Packet();
+		p.setPayload(payload);
+		p.setProtocolVersion(new ProtocolVersion((byte)0));
+		p.setType(PacketType.UNKNOWN);
+		
+		return p;
 	}
-
 }
